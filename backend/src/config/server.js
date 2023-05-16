ba3c98140136d442629ff777271e24a5dc7eab97
Jonathan Lima <jonathanhenriquecwb@gmail.com>
2023-05-16 16:40:31 -0300

    backend - criando as rotas da api

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -1,5 +1,12 @@
 const express = require('express')
 const app = express()
 
+// Middleware
+app.use(express.json())
+app.use(express.urlencoded({ extended: true}))
+
+
 const PORT = 8080
 app.listen(PORT, () => console.log(`Sever is running on port ${PORT}`))
+
+module.exports = app
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,72 @@
-// ES module
-    // import database from "./config/database";
-    // import server from './config/server'
+// Arquivos de configuração
+const app = require('./config/server')
+require('./config/database')
 
-// Comon module
-    require('./config/server')
-    require('./config/database')
+// Chamando model para manipulação no banco de dados
+const Tarefa = require('./models/Tarefa')
+
+
+// PUXA TAREFAS COMPLETAS E POR ID
+app.get('/', function(req, res){
+    Tarefa.find().lean().then(tarefas => {
+        res.send(tarefas)
+    }).catch(err => {
+        res.send('Lista de tarefas vazias')
+    })
+})
+
+// GET
+app.get('/tarefas', function(req, res){
+    Tarefa.find().lean().then(tarefas => {
+        res.send(tarefas)
+    }).catch(err => {
+        res.send('Lista de tarefas não encontradas')
+    })
+})
+
+app.get('/tarefas/:id', (req, res) => {
+    Tarefa.findById({_id: req.params.id}).lean().then(tarefa => {
+        res.send(tarefa)
+    }).catch(err => {
+        res.send('Erro ao encontrar a categoria selecionada')
+    })
+})
+
+// POST
+app.post('/tarefas/create', (req, res) => {
+    const newTask = {
+        description: req.body.description
+    }
+
+    new Tarefa(newTask).save().then(() => {
+        res.send('Tarefa cadastrada com sucesso')
+    }).catch(err => {
+        res.send('Erro ao cadastrar nova tarefa')
+    })
+})
+
+// PUT
+app.put('/tarefas/update', (req, res) => {
+    Tarefa.findById(req.body._id).then(tarefa=> {        
+        tarefa.description = req.body.description
+        tarefa.save().then(() => {
+            res.send('Tarefa auterada com sucesso')
+        })
+    }).catch(err => {
+        res.send('Erro ao tentar alterar registro')
+    })
+})
+
+// DELETE
+app.delete('/tarefas/delete/:id', (req, res) => {
+    Tarefa.deleteOne({_id: req.params.id}).then(() => {
+        res.send('Tarefa deletada com sucesso')
+    }).catch(err => {
+        res.send('Erro ao deletar tarefa')
+    })
+})
+
+
+
+
+
